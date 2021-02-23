72380ea0db49898cea6e1457b35e066366ba524f
menachem margolis <margolismendel5728@gmail.com>
2021-02-23 12:07:47 -0500

    Done.

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [btn, setBtn] = useState('OFF')
+
+  function changeDisplay(e){
+     if(e.target.innerText === "ON"){setBtn( "OFF")}
+     if(e.target.innerText === "OFF"){setBtn( "ON")}
+  }
+
+  return <button onClick = {changeDisplay} >{btn}</button>;
 }
 
 export default Toggle;
+
+// The button should say "OFF" when it is first displayed
+// When the button is clicked, it should say "ON"
+// When the button is clicked again, it should say "OFF"
+// etc
